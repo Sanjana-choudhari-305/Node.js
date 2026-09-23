56208d107dafb4e86401e04c8a758c1207830cce
Sanjana Choudhari <sanjanachoudahrimarch@gmail.com>
2026-09-23 18:55:37 +0530

    Initial Commit

---
+++ demopgm.js
@@ -0,0 +1,25 @@
+console.log("Welcome to nodejs session!")
+
+let a = 67;
+console.log(typeof a);
+
+b = "nodejs example";
+console.log(typeof b);
+
+c = undefined;
+console.log(typeof c);
+
+let d = true;
+console.log("Value: ",d,"| type:", typeof d);
+
+let e = null;
+console.log("Value: ",e,"| type:", typeof e);
+
+let user ={
+    name: "Ammu",
+    age: 21
+};
+console.log("Value: ",user,"| Type: ",typeof user);
+
+let color = ["red","green","blue"];
+console.log("Value: ",color,"|Type: ",typeof color);
