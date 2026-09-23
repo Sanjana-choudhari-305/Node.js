c0020cad911b592883109754a35f1b2258598091
Sanjana Choudhari <sanjanachoudahrimarch@gmail.com>
2026-09-23 19:49:34 +0530

    Function exporting

---
+++ app1.js
@@ -0,0 +1,3 @@
+const circle = require('./circle');
+console.log(`The area of circle is ${circle.area(4)}`);
+console.log(`The circumference of circle is ${circle.circumference(5)}`);
---
+++ circle.js
@@ -0,0 +1,4 @@
+const {PI} = Math;
+exports.area=(r)=>PI*r*r;
+
+exports.circumference=(r)=>2*PI*r;
