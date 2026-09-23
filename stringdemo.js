2fbb93924c728ce2adfdda582b097a8da5d896cb
Sanjana Choudhari <sanjanachoudahrimarch@gmail.com>
2026-09-23 19:19:27 +0530

    changes

---
+++ stringdemo.js
@@ -0,0 +1,12 @@
+let x = "Welcome to infosys";
+let y = "nodejs tutorial";
+let z = ['Infosys','Bangalore'];
+
+console.log(x);
+console.log(y);
+
+console.log("concat using(+): ",(x+y));
+console.log("concat using function: ",(x.concat(y)));
+console.log("Split String: ",x.split(' '));
+console.log("Join string: ",z.join(','));
+console.log("Character at index 5: ",x.charAt(5));
