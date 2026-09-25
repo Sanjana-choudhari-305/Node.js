0b18b7dd88bf4959ae5a557996feb919bb7e69fb
Sanjana Choudhari <sanjanachoudahrimarch@gmail.com>
2026-09-25 18:50:17 +0530

    Some more exports

---
+++ strdemo.js
@@ -0,0 +1,4 @@
+const x = "Welcome to infosys"
+const y = "nodejs session"
+const z = ["Infosys","Bangalore"]
+module.exports={x,y,z};
--- stringApp1.js
+++ stringApp1.js
@@ -1,8 +1,20 @@
-const string = require('./stringdemo');
+// const string = require('./stringdemo');
 
 
-console.log("concat using(+):", string.concatUsingPlus());
-console.log("concat using function:", string.concatUsingFunction());
-console.log("split string:", string.splitString());
-console.log("Join string:", string.joinString());
-console.log("character at index 5:", string.characterAt());
+// console.log("concat using(+):", string.concatUsingPlus());
+// console.log("concat using function:", string.concatUsingFunction());
+// console.log("split string:", string.splitString());
+// console.log("Join string:", string.joinString());
+// console.log("character at index 5:", string.characterAt());
+
+const {x,y,z} = require("./strdemo")
+console.log(x);
+console.log(y);
+
+console.log("Concat using(+): ",x+y);
+console.log("Concat using function: ",x.concat(y));
+
+console.log("Split string: ",x.split(" "));
+console.log("Join string: ",z.join(","));
+
+console.log("Character at index 6: ",x.charAt(6));
